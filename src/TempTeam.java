1c5751a410e123f08fd6663463915fd59bed13fe
131250131 <275838287@qq.com>
2015-03-07 11:19:33 +0800

    TempTeam类初步完成。

--- src/TempTeam.java
+++ src/TempTeam.java
@@ -37,7 +37,7 @@ public class TempTeam {
 	//本场球队总得分数（*****需要更新*****）
 		int teamScores=0;
 	//本场球队比赛时间
-		int teamTime=0;
+		int teamPlayTime=0;
 		
 	//球队的本场球员列表
 		ArrayList<TempPlayer> playlist = new ArrayList<TempPlayer>();
@@ -45,8 +45,23 @@ public class TempTeam {
 	//计算本场球队的总数居
 		void sumOfData(){
 			for(TempPlayer tempPlayer : playlist){
-				
-				
+				//this.attackRound =     ;
+				this.teamAssists += tempPlayer.playerAssists;
+				this.teamBlocks += tempPlayer.playerBlocks;
+				this.teamOffenceRebounds += tempPlayer.playerOffenceRebounds;
+				this.teamDeffenceRebounds += tempPlayer.playerOffenceRebounds;
+				this.teamTotalRebounds += tempPlayer.playerTotalRebounds;
+				this.teamFG += tempPlayer.playerFG;
+				this.teamFGTry += tempPlayer.playerFGTry;
+				this.team3FG += tempPlayer.player3FG;
+				this.team3FGTry += tempPlayer.player3FGTry;
+				this.teamFouls += tempPlayer.playerFouls;
+				this.teamFTG += tempPlayer.playerFTG;
+				this.teamFGTry += tempPlayer.playerFTGTry;
+				this.teamSteals += tempPlayer.playerSteals;
+				this.teamTurnovers += tempPlayer.playerTurnovers;
+				this.teamScores += tempPlayer.playerScores;
+				this.teamPlayTime += tempPlayer.playerPlayTime;
 			}
 		}
 
